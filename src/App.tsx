2e3811c5b00de15a7c17f78d994d3b2b70113098
Artemy <artemij87work@gmail.com>
2022-03-30 20:20:07 +0300

    upd ts

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React, {useState} from 'react';
 import './App.css';
-import {Todolist} from './Todolist';
+import {TaskType, Todolist} from './Todolist';
 import {v1} from 'uuid';
 
 export type FilterValuesType = "all" | "active" | "completed";
@@ -9,6 +9,10 @@ type TodolistsType = {
     id: string,
     title: string
     filter: FilterValuesType
+}
+
+type TasksType = {
+    [key: string]: TaskType[]
 }
 
 function App() {
@@ -36,7 +40,7 @@ function App() {
         {id: todolistID2, title: 'What to buy', filter: 'all'},
     ])
 
-    let [tasks, setTasks] = useState({
+    let [tasks, setTasks] = useState<TasksType>({
         [todolistID1]:[
             {id: v1(), title: "HTML&CSS", isDone: true},
             {id: v1(), title: "JS", isDone: true},
@@ -78,16 +82,7 @@ function App() {
     }
 
     function changeStatus(todolistID:string, taskId: string, isDoneValue: boolean) {
-        // let task = tasks.find(el => el.id === todolistID)
-        // let task = tasks.find(t => t.id === taskId);
-        // if (task) {
-        //     task.isDone = isDone;
-        // }
-        //
-        // setTasks([...tasks]);
-
-        setTasks(
-            {
+        setTasks({
                 ...tasks,
                 [todolistID]: tasks[todolistID].map(el => el.id ===taskId ? {...el, isDone: isDoneValue} : el)
             }
